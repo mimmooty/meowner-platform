0d046911bf91c6caa3d2495fcb0a0c5e3f441429
mimmooty <sasithornsupamarkpukdee@gmail.com>
2026-01-20 22:06:05 +0700

    add userservice

---
+++ services/user/src/main/kotlin/com/meowner/user/controller/UserController.kt
@@ -0,0 +1,13 @@
+package com.meowner.user.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController {
+
+    @GetMapping("/hello")
+    fun sayHello(): String {
+        return "Hello from Kotlin Spring Boot 2026!"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "meowner-platform"
+include("services:auth-service")
+include("libs:common-security") // เผื่อไว้ใช้แชร์โค้ด Security ในอนาคต
